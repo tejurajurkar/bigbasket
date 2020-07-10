a6cf320eba632382f379d8789eee085ab8ae432f
tejurajurkar <tejurajurkar@gmail.com>
2020-07-10 17:08:25 +0530

    first comit

---
+++ BigBasket/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using BigBasket.Resources;
+using SQLite;
+using System.IO;
+
+namespace BigBasket
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText txtemail;
+        EditText txtPassword;
+        Button btncreate;
+        Button btnsign;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource  
+            SetContentView(Resource.Layout.activity_main);
+            // Get our button from the layout resource,  
+            // and attach an event to it  
+            btnsign = FindViewById<Button>(Resource.Id.btnlogin);
+            btncreate = FindViewById<Button>(Resource.Id.btnregister);
+            txtemail = FindViewById<EditText>(Resource.Id.txt_reg_email);
+            txtPassword = FindViewById<EditText>(Resource.Id.txt_reg_password);
+            btnsign.Click += Btnsign_Click;
+            btncreate.Click += Btncreate_Click;
+            CreateDB();
+        }
+        private void Btncreate_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+        private void Btnsign_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dpPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "user.db3"); //Call Database  
+                var db = new SQLiteConnection(dpPath);
+                var data = db.Table<LoginTable>(); //Call Table  
+                var data1 = data.Where(x => x.Email == txtemail.Text && x.Password == txtPassword.Text).FirstOrDefault(); //Linq Query  
+                if (data1 != null)
+                {
+                    Toast.MakeText(this, "Login Success", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Email or Password invalid", ToastLength.Short).Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.ToString(), ToastLength.Short).Show();
+            }
+        }
+        public string CreateDB()
+        {
+            var output = "";
+            output += "Creating Databse if it doesnt exists";
+            string dpPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "user.db3"); //Create New Database  
+            var db = new SQLiteConnection(dpPath);
+            output += "\n Database Created....";
+            return output;
+        }
+    }
+}
---
+++ BigBasket/Resources/LoginTable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace BigBasket.Resources
+{
+    class LoginTable
+    {
+        [PrimaryKey, AutoIncrement, Column("_Id")]
+
+        public int Id { get; set; } // AutoIncrement and set primarykey  
+
+        [MaxLength(25)]
+
+        public string Email { get; set; }
+
+        [MaxLength(15)]
+
+        public string Password { get; set; }
+    }
+}
